db8209b21851763eb7a537aa667235a0880db483
Tomas <tomascorvalan14@gmail.com>
2026-02-01 12:01:37 -0300

    añadido instagram

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,21 @@
 const Footer = () => {
-  return <div className="Footer">© Jabones Artesanales</div>;
+  return (
+    <div className="footer">
+      <span>© Jabones Artesanales</span>
+
+      <a
+        href="https://www.instagram.com/jabones_y_amores/"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="instagram-link"
+      >
+        <img
+          src="https://cdn-icons-png.flaticon.com/512/2111/2111463.png"
+          alt="Instagram"
+        />
+      </a>
+    </div>
+  );
 };
 
 export default Footer;
